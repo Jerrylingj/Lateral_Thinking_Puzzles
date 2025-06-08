ef24a5356eec1c0c75b4a8475344b85cea15169d
jerrylingj <498539230@qq.com>
2025-06-08 16:10:44 +0800

    test1

--- frontend/src/config/env.js
+++
@@ -1,9 +0,0 @@
-// 环境配置文件
-module.exports = {
-    // 是否启用云端环境
-    enableCloud: true,
-    // 本地环境API地址
-    localApiUrl: 'http://localhost:3000',
-    // 云端环境API地址
-    cloudApiUrl: 'http://game.jerrylingj.top'
-  }
